58ef1db2d5b2f6e6ae3816926a2700f954534f12
Slawek Zachcial <slawek.zachcial@hp.com>
2024-09-07 12:17:12 +0200

    Slawek: update nvim-cmp setup to just override NvChad config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,15 +46,17 @@ return {
         end,
       },
     },
-    opts = {
-      sources = {
-        { name = "copilot", group_index = 2 },
-        { name = "nvim_lsp", group_index = 2 },
-        { name = "buffer", group_index = 2 },
-        { name = "path", group_index = 2 },
-        { name = "luasnip", group_index = 2 },
-      },
-    },
+    config = function()
+      local opts = require "nvchad.configs.cmp"
+      opts.sources = {
+        { name = "copilot" },
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+        { name = "luasnip" },
+      }
+      require("cmp").setup(opts)
+    end,
   },
 
   {
