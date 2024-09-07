36e68206526aec98cead703a176c4156465a0745
Slawek Zachcial <slawek.zachcial@hp.com>
2024-09-07 11:38:14 +0200

    Slawek: working GitHub copilot setup with nvim-cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -13,25 +13,54 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
-
   {
-    'github/copilot.vim',
-    lazy = false
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "markdown",
+        -- "vim", "lua", "vimdoc",
+        --   "html", "css"
+      },
+    },
+  },
+
+  -- Sane setup of GitHub Copilot with CMP: https://tamerlan.dev/setting-up-copilot-in-neovim-with-sane-settings/
+  {
+    "zbirenbaum/copilot.lua",
+    lazy = false,
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enable = false },
+      panel = { enable = false },
+    },
   },
 
   {
-    'nvim-telescope/telescope.nvim',
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "copilot", group_index = 2 },
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "buffer", group_index = 2 },
+        { name = "path", group_index = 2 },
+        { name = "luasnip", group_index = 2 },
+      },
+    },
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
     config = function()
-      require('telescope').setup {
+      require("telescope").setup {
         defaults = {
           -- mappings = {
           --   i = {
@@ -39,14 +68,27 @@ return {
           --     ['<C-d>'] = false,
           --   },
           -- },
-          vimgrep_arguments = { 'rg', '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '--hidden', '--glob', '!**/.git/**', '--glob', '!**/.terraform/**' },
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+            "--glob",
+            "!**/.git/**",
+            "--glob",
+            "!**/.terraform/**",
+          },
         },
         pickers = {
           find_files = {
-            find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/**', '--glob', '!**/.terraform/**' },
-          }
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/**", "--glob", "!**/.terraform/**" },
+          },
         },
       }
     end,
-  }
+  },
 }
